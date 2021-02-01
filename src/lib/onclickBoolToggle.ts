17ba8db53e7b9b3a14668bc32a52df7f0a8c377d
panManfredini <pan.manfredini@gmail.com>
2021-02-01 18:21:17 +0100

    adding click toggle

---
+++ src/lib/onclickBoolToggle.ts
@@ -0,0 +1,17 @@
+import {hmiElement,html} from "jahmin"
+
+export class onclickToggle extends hmiElement {
+    constructor(){
+        super();
+        this.addEventListener("click", this.toggle.bind(this));
+    }
+    toggle(){
+        let toggle = this.value ? false : true;
+        this.Write(toggle);
+    }
+    render(){
+        return html`<slot></slot>`
+    }
+}
+customElements.define("onclick-toggle",onclickToggle);
+
